b3ee001ca3013d4ad22617a697d28370ff177b80
ArminSalai <kuka.kari12@gmail.com>
2021-08-09 15:48:44 +0200

    modelled 2 docks, and added their respective links and pages

---
+++ assets/dock.js
@@ -0,0 +1,89 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js';
+import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js';
+import {RectAreaLightUniformsLib} from 'https://threejs.org/examples/jsm/lights/RectAreaLightUniformsLib.js';
+
+const canvas = document.querySelector(".webgl");
+const scene = new THREE.Scene();
+
+let width = window.innerWidth * 0.75;
+let height = window.innerHeight * 0.75;
+
+const camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 100);
+camera.position.set(25, 18, 45);
+camera.lookAt(0, 0, 0);
+camera.zoom = 0.5;
+camera.updateProjectionMatrix();
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    alpha: true,
+    antialias: true
+});
+
+RectAreaLightUniformsLib.init();
+
+scene.background = null;
+
+renderer.setSize(width, height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.shadowMap = true;
+
+const constrols = new OrbitControls(camera, renderer.domElement);
+constrols.enableZoom = false;
+constrols.enablePan = false;
+
+const gLight = new THREE.PointLight(0x979DA6, 19 / 4, 300);
+gLight.position.set(19, 10, 14);
+gLight.castShadow = true;
+scene.add(gLight);
+
+const yLight = new THREE.PointLight(0xF2EED3, 10 / 4, 200);
+yLight.position.set(-10, 12, -23);
+yLight.castShadow = true;
+scene.add(yLight);
+
+const roughnessLight1 = new THREE.SpotLight(0xffffff, 5 / 8, 40, 80, 0, 1);
+roughnessLight1.position.set(0, -10, -18);
+scene.add(roughnessLight1);
+
+const roughnessLight2 = new THREE.SpotLight(0xffffff, 5 / 4, 40, 80, 0, 1);
+roughnessLight2.position.set(0, -10, 20);
+roughnessLight2.rotateOnWorldAxis("x", Math.PI);
+scene.add(roughnessLight2);
+
+const backLight = new THREE.SpotLight(0xffffff, 5 / 2, 40, 80, 0, 1);
+backLight.position.set(0, -10, 10);
+scene.add(backLight);
+
+const hLight = new THREE.HemisphereLight(0xF2D64B, 0x68788C, 0.6);
+scene.add(hLight);
+
+const portLight = new THREE.PointLight(0xFFFFFF, 2, 200);
+portLight.position.set(-20, -2, 40);
+scene.add(portLight);
+
+
+const areaLight = new THREE.RectAreaLight(0xffffff, 20, 14, 14);
+areaLight.position.set(-5, 10, -5);
+areaLight.lookAt(0, 0, 0);
+scene.add(areaLight);
+
+var mobil;
+
+const loader = new GLTFLoader()
+loader.load("assets/models/dock.glb", function(glb) {
+    mobil = glb.scene;
+    scene.add(mobil);
+    mobil.rotation.y = -2.3;
+}, function(xhr) {
+    console.log(xhr.loaded / xhr.total * 100 + "% betolve");
+});
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
